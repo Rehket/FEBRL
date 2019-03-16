1b1272872e7796a23ab30cf206d44dc1893a1178
Adam Albright <aalbright425@gmail.com>
2019-03-15 22:56:10 -0400

    Converting to Python 3

--- trainhmm.py
+++ trainhmm.py
@@ -1,29 +1,29 @@
 # =============================================================================
 # AUSTRALIAN NATIONAL UNIVERSITY OPEN SOURCE LICENSE (ANUOS LICENSE)
 # VERSION 1.3
-# 
+#
 # The contents of this file are subject to the ANUOS License Version 1.3
 # (the "License"); you may not use this file except in compliance with
 # the License. You may obtain a copy of the License at:
-# 
+#
 #   https://sourceforge.net/projects/febrl/
-# 
+#
 # Software distributed under the License is distributed on an "AS IS"
 # basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
 # the License for the specific language governing rights and limitations
 # under the License.
-# 
+#
 # The Original Software is: "trainhmm.py"
-# 
+#
 # The Initial Developer of the Original Software is:
 #   Dr Peter Christen (Research School of Computer Science, The Australian
 #                      National University)
-# 
+#
 # Copyright (C) 2002 - 2011 the Australian National University and
 # others. All Rights Reserved.
-# 
+#
 # Contributors:
-# 
+#
 # Alternatively, the contents of this file may be used under the terms
 # of the GNU General Public License Version 2 or later (the "GPL"), in
 # which case the provisions of the GPL are applicable instead of those
@@ -107,75 +107,78 @@ import simplehmm
 # =============================================================================
 # Get command line arguments
 
-if (len(sys.argv) != 4):
-  print 'USAGE: python trainhmm.py  [hmm_training_file] [hmm_model_file] ' + \
-        '[hmm_smoothing]'
-  sys.exit()
+if len(sys.argv) != 4:
+    print(
+        "USAGE: python trainhmm.py  [hmm_training_file] [hmm_model_file] "
+        + "[hmm_smoothing]"
+    )
+    sys.exit()
 
 hmm_training_file = sys.argv[1]
-hmm_model_file =    sys.argv[2]
-hmm_smoothing =     sys.argv[3].lower()
+hmm_model_file = sys.argv[2]
+hmm_smoothing = sys.argv[3].lower()
 
-if (hmm_smoothing not in ['none', 'laplace','absdiscount']):
-  print 'Illegal HMM smoothing method: %s ' % (hmm_smoothing) + \
-        '(has to be one of: "none", "laplace","absdiscount")'
-  sys.exit()
+if hmm_smoothing not in ["none", "laplace", "absdiscount"]:
+    print(
+        "Illegal HMM smoothing method: %s " % (hmm_smoothing)
+        + '(has to be one of: "none", "laplace","absdiscount")'
+    )
+    sys.exit()
 
-if (hmm_smoothing == 'none'):
-  hmm_smoothing = None
+if hmm_smoothing == "none":
+    hmm_smoothing = None
 
 # =============================================================================
 # Load HMM training file, chek its structure and get all tags and HMM states
 
 try:
-  train_file = open(hmm_training_file, 'r')
+    train_file = open(hmm_training_file, "r")
 except:
-  logging.exception('Cannot open HMM training file: "%s"' % \
-                    (hmm_training_file))
-  raise IOError
+    logging.exception('Cannot open HMM training file: "%s"' % (hmm_training_file))
+    raise IOError
 
-tag_set =        set()
-state_set =      set()
+tag_set = set()
+state_set = set()
 train_rec_list = []
 
 line_cnt = 0
 
 line = train_file.readline()
-while (line != ''):
-  line = line.strip()
+while line != "":
+    line = line.strip()
 
-  if (line.startswith('#STOP')):
-    break  # End processing training records
+    if line.startswith("#STOP"):
+        break  # End processing training records
 
-  if ((line != '') and (line[0] != '#')):  # Line not empty and not a comment
+    if (line != "") and (line[0] != "#"):  # Line not empty and not a comment
 
-    if ('#' in line):
-      line = line[:line.find('#')].strip()  # Remove comment from end of line
+        if "#" in line:
+            line = line[: line.find("#")].strip()  # Remove comment from end of line
 
-    train_rec = []
+        train_rec = []
 
-    for tag_state_pair in line.split(','):
-      tag, state = tag_state_pair.split(':')
+        for tag_state_pair in line.split(","):
+            tag, state = tag_state_pair.split(":")
 
-      tag =   tag.strip()
-      state = state.strip()
+            tag = tag.strip()
+            state = state.strip()
 
-      if ((len(tag) != 2) or (tag.isupper() != True)):
-        print 'Illegal tag in line %d: %s' % (line_cnt, line)
-        sys.exit()
-      tag_set.add(tag)
+            if (len(tag) != 2) or (tag.isupper() != True):
+                print("Illegal tag in line %d: %s" % (line_cnt, line))
+                sys.exit()
+            tag_set.add(tag)
 
-      if ((state == '') or (state.isupper() == True)):
-        print 'Empty or illegal state in line %d: %s' % (line_cnt, line)
-        sys.exit()
-      state_set.add(state)
+            if (state == "") or (state.isupper() == True):
+                print("Empty or illegal state in line %d: %s" % (line_cnt, line))
+                sys.exit()
+            state_set.add(state)
 
-      train_rec.append((state, tag))
+            train_rec.append((state, tag))
 
-    train_rec_list.append(train_rec)
+        train_rec_list.append(train_rec)
 
-  line_cnt += 1
-  line = train_file.readline()
+    line_cnt += 1
+    line = train_file.readline()
 
 train_file.close()
 
@@ -185,21 +188,21 @@ state_list = list(state_set)
 state_list.sort()
 
 
-print 'Set of tags found in HMM training file:'
-print '  %s' % (', '.join(tag_list))
-print
-print 'Set of HMM states found in HMM training file:'
-print '  %s' % (', '.join(state_list))
-print
+print("Set of tags found in HMM training file:")
+print("  %s" % (", ".join(tag_list)))
+print()
+print("Set of HMM states found in HMM training file:")
+print("  %s" % (", ".join(state_list)))
+print()
 
-print 'Parsed %d training records:' % (len(train_rec_list))
+print("Parsed %d training records:" % (len(train_rec_list)))
 for train_rec in train_rec_list:
-  print '  %s' % (train_rec)
-print
+    print("  %s" % (train_rec))
+print()
 
 # Initalise HMM and train it with training data - - - - - - - - - - - - - - - -
 #
-hmm_name =   'Febrl HMM based on training file "%s"' % (hmm_training_file)
+hmm_name = 'Febrl HMM based on training file "%s"' % (hmm_training_file)
 hmm_states = list(state_set)
 hmm_observ = list(tag_set)
 
