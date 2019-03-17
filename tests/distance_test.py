eeb640bd7fbec73c9219bce4474918c68e24514a
Adam Albright <aalbright425@gmail.com>
2019-03-16 21:54:20 -0400

    Moving distance functions into their own file.

---
+++ tests/distance_test.py
@@ -0,0 +1,56 @@
+
+from febrl import distance
+import pytest
+
+def test_distL1():
+    """
+    - distL1 should raise a Value Exception if vector lengths are not the same.
+    - For a vectors (0, 0) and (0, 0), result should be 0
+    - For a vectors (0, 0) and (0, 5), result should be 5
+    - For a vectors (-3, -3) and (3, 3), result should be 12
+
+    :return:
+    """
+
+    assert distance.distL1([0, 0], [0, 0]) == 0
+    assert distance.distL1([0, 0], [0, 5]) == 5
+    assert distance.distL1([-3, -3], [3, 3]) == 12
+
+    with pytest.raises(ValueError) as e_info:
+        distance.distL1([0, 0], [0, 0, 0])
+
+
+def test_distL2():
+    """
+    - distL2 should raise a Value Exception if vector lengths are not the same.
+    - For a vectors (0, 0) and (0, 0), result should be 0
+    - For a vectors (0, 0) and (0, 5), result should be 5
+    - For a vectors (-3, -3) and (3, 3), result should be 8.4853
+
+    :return:
+    """
+
+    assert distance.distL2([0, 0], [0, 0]) == 0
+    assert distance.distL2([0, 0], [0, 5]) == 5
+    assert round(distance.distL2([-3, -3], [3, 3]), 4) == 8.4853  # 8.4852813742385702928101323452582
+
+    with pytest.raises(ValueError) as e_info:
+        distance.distL2([0, 0], [0, 0, 0])
+
+
+def test_distLInf():
+    """
+    - distL2 should raise a Value Exception if vector lengths are not the same.
+    - For a vectors (0, 0) and (0, 0), result should be 0
+    - For a vectors (0, 0) and (0, 5), result should be 5
+    - For a vectors (-3, -3) and (3, 3), result should be 8.4853
+
+    :return:
+    """
+
+    assert distance.distL2([0, 0], [0, 0]) == 0
+    assert distance.distL2([0, 0], [0, 5]) == 5
+    assert round(distance.distL2([-3, -3], [3, 3]), 4) == 8.4853  # 8.4852813742385702928101323452582
+
+    with pytest.raises(ValueError) as e_info:
+        distance.distL2([0, 0], [0, 0, 0])
