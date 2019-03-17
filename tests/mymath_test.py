3352209206c2b8d989bc4c683254ce93f11fc827
Adam Albright <aalbright425@gmail.com>
2019-03-16 21:27:09 -0400

    Rebuilding the tests.

---
+++ tests/mymath_test.py
@@ -0,0 +1,18 @@
+
+from febrl import mymath
+
+
+
+def test_distL1():
+    """
+    - distL1 should raise a Value Exception if lengths are not the same.
+    - For a vectors (0, 0) and (0, 0), result should be 0
+    - For a vectors (0, 0) and (0, 5), result should be 5
+    - For a vectors (-3, -3) and (3, 3), result should be 12
+
+    :return:
+    """
+
+    assert mymath.distL1([0, 0], [0, 0]) == 0
+    assert mymath.distL1([0, 0], [0, 5]) == 5
+    assert mymath.distL1([-3, -3], [3, 3]) == 12
